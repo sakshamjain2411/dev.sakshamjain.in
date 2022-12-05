7e9d2e58f28bd962f6e073d2b122a8a9b1cef844
Jain <saksham.jain@sap.com>
2022-12-05 16:14:59 +0530

    SCCM Email Config

--- sccm/send_mail.php
+++ sccm/send_mail.php
@@ -1,11 +1,12 @@
 <?php
     function SendMail($data, $subject) {
         $message = "";
+        $headers  = "From: " . "info@sakshamjain.in" . "\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
         foreach ($data as $key => $value) { 
             $message = $message . ucwords($key)." : ".$value."<br>";
         }
-        // echo $subject;
-        // echo $message;
-        mail("saksham.jain1998@gmail.com",$subject,$message);
+        mail("saksham.jain1998@gmail.com",$subject,$message,$headers);
     }
 ?>
